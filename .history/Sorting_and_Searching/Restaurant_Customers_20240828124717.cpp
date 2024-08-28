66ede7c9d36188984cd03eca6594bafe8c84c61b
Essam <hamaliessam2002@gmail.com>
2024-08-28 13:03:06 +0600

    Resturant_Customers_Added

---
+++ .history/Sorting_and_Searching/Restaurant_Customers_20240828124717.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(v)      ((v).begin()),((v).end())
+#define sz(v)       ((int)((v).size()))
+#define clr(v,d)    memset(v,d,sizeof(v))
+#define rep(i,v)    for(ll i=0; i<sz(v); i++)
+#define lp(i,n)     for(ll i=0; i<(int)(n); ++i)
+#define lpi(i,j,n)  for(ll i=(j); i<(int)(n); ++i)
+#define lpd(i,j,n)  for(ll i=(j); i>=(int)(n); --i)
+#define ln '\n'
+typedef long long ll;
+
+int main() {
+    int n;
+    vector <pair<int,int>> events;
+    lp(i,n){
+        int 
+    }
+    return 0;
+}
